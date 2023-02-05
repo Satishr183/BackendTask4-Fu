3049f6194ebe43f610eadec92cbe35d2bdb3a86c
Satish <satish_95raj@outlook.com>
2023-02-05 07:19:07 +0530

    authorization initiated

--- src/controllers/customerController.js
+++ src/controllers/customerController.js
@@ -1,8 +1,5 @@
 const customerModel = require('../models/customerModel')
 
-
-
-
 const newCustomer = async function(req, res){   
     try{
         const {fname, lname, mobileNumber, DOB, emailID, address, status } = req.body
@@ -15,6 +12,41 @@ const newCustomer = async function(req, res){
     }
 }
 
+const customerLogin = async function(req, res){
+    try{
+       const emailID = req.body.emailID
+
+        if (!emailID) {
+            return res
+              .status(400)
+              .send({ status: false, message: "please provide an Email !" });
+          }
+
+          let email = await userModel.findOne({ emailID: emailID })
+
+          if (!email) {
+            return res.status(400).send({ status: false, message: "emailID  is not corerct" })
+          } 
+
+          
+    let token = jwt.sign(
+        {
+          customerID: email._id.toString()
+        },
+        process.env.SECRET_KEY,
+        { expiresIn: "2h" }
+      );
+      let Token = {
+        customerID: email._id.toString(),
+        token: token
+       }
+      res.setHeader("Authorization", token);
+      res.status(200).send({ status: true, message: "Success" });
+    
+    }catch(err){
+        res.status(500).send({status:false, message:err.message})
+    }
+}
 
 const getAllCustomers = async function(req,res){
     const getAll = await customerModel.find({status:"ACTIVE"})
@@ -27,4 +59,4 @@ const deleteByID = async function(req,res){
     deleteCustomer = await customerModel.findByIdAndUpdate({_id:customerId},status,{new:true})
 }
 
-module.exports = {newCustomer,getAllCustomers,deleteByID}
+module.exports = {newCustomer, customerLogin, getAllCustomers, deleteByID}
---
+++ src/middleware/auth.js
@@ -0,0 +1,57 @@
+
+
+const jwt = require('jsonwebtoken');
+const customerModel = require('../models/customerModel');
+
+const authentication = async function (req, res, next) {
+    try {
+        let token = req.header("Authorization")
+        if (!(token)) {
+            return res.status(401).send({ status: false, msg: "Token must be entered" })
+        }
+        const bearer = token.split(' ');
+        const bearerToken = bearer[1];
+        let decodedtoken = jwt.verify(bearerToken, "functionup-plutonium")
+        if (!(decodedtoken)){
+            return res.status(401).send({ status: false, msg: "Invalid Token" })
+        } 
+        req.decodedToken = decodedtoken
+        res.setHeader("Authorization", token)
+        next()
+    }
+    catch (err) {
+        if (err.name === "JsonWebTokenError" || err.message==="jwt expired") {
+            res.status(401).send({  status: false, msg: err.message });
+          } else return res.status(500).send({  status: false, msg: err.message });
+    }
+}
+
+const authorization = async function(req, res,next){
+try{    
+    const decoded = req.decodedToken
+    const customerID= req.body.customerID
+
+    if(customerID)
+    {
+        if(!(customerID.match(/^[0-9a-fA-F]{24}$/)))
+        return res.status(400).send({status:false,message:"Invalid customerID given"})
+        
+        const customer = await customerModel.findById(customerID)
+        
+        if(!customer)
+        return res.status(404).send({status:false,message:"customer not found !"})
+
+        if(decoded.customerID !== customerID.toString())
+        return res.status(403).send({status:false,message:"Unauthorised access"})
+
+    }else
+        return res.status(400).send({status:false,message:"customerID Required"})
+}catch(err){
+    return res.status(500).send({status:false, message:err.message})
+}
+next()
+
+    }
+
+
+module.exports={authentication, authorization}
